f8a381827ba83873d02901c3e9205b864a908256
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-02-18 17:26:27 +0500

    Remove commands to Executor class

---
+++ TuringMachine/Executor.cs
@@ -0,0 +1,59 @@
+﻿namespace Turing;
+
+public static class Executor
+{
+    private static Dictionary<string, Action<TuringMachine, string[]>> allCommands;
+
+    static Executor()
+    {
+        allCommands = new();
+        allCommands.Add("step", ((machine, args) => machine.NextStep()));
+        allCommands.Add("cls", ((machine, args) => Console.Clear()));
+        allCommands.Add("settape", (machine, args) =>
+        {
+            if (args.Length != 1)
+                throw new ArgumentException("Wrong arguments count");
+            var tape = args[0];
+            machine.InsertTape(tape);
+        });
+        allCommands.Add("setinitstate", (machine, args) =>
+        {
+            if (args.Length != 1)
+                throw new ArgumentException("Wrong arguments count");
+            var state = args[0];
+            machine.SetInitialState(state);
+        });
+        allCommands.Add("show", (machine, args) =>
+        {
+            var clip = machine.TapeClipping(0, 10);
+            Console.WriteLine(clip);
+        });
+        allCommands.Add("setrule", (machine, args) =>
+        {
+            var ruleExpression = String.Join(' ', args);
+            Rule rule;
+            if (!new Parser().TryParse(ruleExpression, out rule))
+                throw new Exception("Invalid rule syntax");//TODO: заменить все исключения
+            machine.UpdateRules(rule);
+        });
+        allCommands.Add("exit", (machine, args) => Environment.Exit(0));
+        /*
+        case "start":
+                //TODO: start machine
+                break;
+            case "stop":
+                //TODO: stop machine
+                break;
+        */
+    }
+
+    public static void Excecute(TuringMachine machine, string command)
+    {
+        var commandProperties = command.Split();//TODO: rename
+        var commandName = commandProperties[0];
+        var commandArgs = commandProperties.Skip(1).ToArray();
+        if (!allCommands.ContainsKey(commandName))
+            throw new Exception("Unknown command.");
+        allCommands[commandName].Invoke(machine, commandArgs);
+    }
+}
--- TuringMachine/Program.cs
+++ TuringMachine/Program.cs
@@ -8,50 +8,14 @@ namespace Turing
     {
         static void Main(string[] args)
         {
-            var parser = new Parser();
             var machine = new TuringMachine();
-            Rule rule;
             while (true)
             {
                 Console.Write("> ");
                 var line = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(line))
-                    return;
-                switch (line)
-                {
-                    case "start":
-                        //TODO: start machine
-                        break;
-                    case "stop":
-                        //TODO: stop machine
-                        break;
-                    case "step":
-                        machine.NextStep();
-                        continue;
-                    case "settape":
-                        Console.Write("Enter tape:");
-                        var tape = Console.ReadLine();
-                        machine.InsertTape(tape);
-                        continue;
-                    case "setinitstate":
-                        Console.Write("Enter initial state:");
-                        var state = Console.ReadLine();
-                        machine.SetInitialState(state);
-                        continue;
-                    case "show":
-                        var clip = machine.TapeClipping(0, 10);
-                        Console.WriteLine(clip);
-                        continue;
-                    case "cls":
-                        Console.Clear();
-                        continue;
-                    case "exit":
-                        Environment.Exit(0);
-                        break;
-                }
-
-                if (parser.TryParse(line, out rule))
-                    machine.UpdateRules(rule);
+                    continue;
+                Executor.Excecute(machine, line);
             }
         }
     }
